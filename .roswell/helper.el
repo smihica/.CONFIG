588191a60bb9c7e452a59c8028f66803e31acf29
smihica <smihica@gmail.com>
2018-04-12 18:29:53 +0900

    Added roswell

---
+++ .roswell/helper.el
@@ -0,0 +1,28 @@
+(defun roswell-configdir ()
+  (substring (shell-command-to-string "ros roswell-internal-use version confdir") 0 -1))
+
+(defun roswell-opt (var)
+  (with-temp-buffer
+    (insert-file-contents (concat (roswell-configdir) "config"))
+    (goto-char (point-min))
+    (re-search-forward (concat "^" var "\t[^\t]+\t\\(.*\\)$"))
+    (match-string 1)))
+
+(defun roswell-slime-directory ()
+  (concat
+   (roswell-configdir)
+   "lisp/slime/"
+   (roswell-opt "slime.version")
+   "/"))
+
+(defvar roswell-slime-contribs '(slime-fancy))
+
+(let* ((slime-directory (roswell-slime-directory)))
+  (add-to-list 'load-path slime-directory)
+  (require 'slime-autoloads)
+  (setq slime-backend (expand-file-name "swank-loader.lisp"
+                                        slime-directory))
+  (setq slime-path slime-directory)
+  (slime-setup roswell-slime-contribs))
+
+(setq inferior-lisp-program "ros run")
